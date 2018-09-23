9310bc748088da36d18366c8629ec64db0383663
CarlosEncisoUSMP <carlos_enciso@usmp.pe>
2018-09-22 19:11:57 -0500

    first commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace RepositorioCE.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
